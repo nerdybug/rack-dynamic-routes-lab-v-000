dd7df32ece757ef6fdd3135f1f7f8aac241a35cd
Meredith Reece <mardebugger@gmail.com>
2018-01-15 20:20:12 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      if item = @@items.detect {|i| i.name == item_name}
+        resp.write "#{item.price}"
+      end
+    end
+  end
+end
